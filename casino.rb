d2b48eb6a611918de7a1e3971e0ef5bcf0dfcaae
Tom Lynam <tom.s.lynam@gmail.com>
2016-05-17 14:12:48 -0600

    separate payout methods

--- casino.rb
+++ casino.rb
@@ -20,38 +20,49 @@ class Casino
 	    		# plays high low
 	    		@high_low = HighLow.new
 	    		@high_low.deal_cards
+	    		payout_high_low
 	    	when '2'
 	    		@slots = Slots.new
 	    		@slots.pull
 	    		# help us reset the instance of our game
 	    		# because high_low is already instantiated
 	    		# so it always hits the 'if' in payout
-	    		@high_low = nil
+	    		payout_slots
 	    	else
 	    		puts "Goodbye!"
 	    		exit
     	end
     end
 
-    def payout
-    	if @high_low
-    		if @high_low.player_wins == true 
-    			@player.bank_roll = @player.bank_roll + @high_low.wager 
-    			puts "Your new bank roll is #{@player.bank_roll}."
-    		elsif @high_low.player_wins == false
-    			  @player.bank_roll = @player.bank_roll - @high_low.wager
-    			  puts "Your new bank roll is #{@player.bank_roll}."	
-    		end
-    	else @slots
-    		if @slots.player_wins == true
-    			@player.bank_roll = @player.bank_roll + @slots.wager 
-    			puts "Your new bank roll is #{@player.bank_roll}."
-	    	elsif @slots.player_wins == false
-	    		  @player.bank_roll = @player.bank_roll - @slots.wager 
-	    		  puts "Your new bank roll is #{@player.bank_roll}."
-			end
+    def payout_high_low
+		if @high_low.player_wins == true 
+			@player.bank_roll = @player.bank_roll + @high_low.wager 
+			printf("Your new bank roll is $%.2f.\n", @player.bank_roll)
+		elsif @high_low.player_wins == false
+			  @player.bank_roll = @player.bank_roll - @high_low.wager
+			  printf("Your new bank roll is $%.2f.\n", @player.bank_roll)
+		end
+    	if @player.bank_roll == 0 
+    		exit(0) 
     	end
-    	#bank_roll == 0 ? exit : 
+
+	    	puts "Would you like to play again? (y/n)"
+			user_input = gets.strip.downcase
+		# TODO put this on one line
+		if user_input == 'y'
+			hit_the_tables
+		else
+		end
+    end
+
+  def payout_slots
+		if @slots.player_wins == true
+			@player.bank_roll = @player.bank_roll + @slots.wager 
+			printf("Your new bank roll is $%.2f.\n", @player.bank_roll)
+    	elsif @slots.player_wins == false
+    		  @player.bank_roll = @player.bank_roll - @slots.wager 
+    		  printf("Your new bank roll is $%.2f.\n", @player.bank_roll)
+		end
     	if @player.bank_roll == 0 
     		exit(0) 
     	end
@@ -68,7 +79,6 @@ class Casino
     	
     def hit_the_tables
     	pick_game
-    	payout
     end
 end
 
@@ -78,17 +88,24 @@ class Player
 
 	def initialize
 		@name = name
-		@bank_roll = 200.00
+		@bank_roll = 200
 		intro
 	end
 
 	def intro
 		puts "Welcome to the casino. What's your name?"
 		@name = gets.strip
-		# TODO number to currency
-		puts "Good luck #{@name}, you have #{@bank_roll} to play with."
+		printf("Good luck %s, you have $%.2f to play with.\n", @name, @bank_roll)
+
 	end
 
 end
 
 Casino.new.hit_the_tables
+
+
+# TO DO list
+# break out payouts into different methods 
+# create a cashier class
+# ask for bankroll, set user input equal to bankroll
+# dynamic wager (choose how much to bet, give option for all in)
