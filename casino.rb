0c66eb1ab7c37ec746e3c8bed362d720210130b5
Tom Lynam <tom.s.lynam@gmail.com>
2016-05-17 15:46:57 -0600

    with cashier class

--- casino.rb
+++ casino.rb
@@ -17,69 +17,60 @@ class Casino
 		puts "2: Slots"
 		case gets.strip
 	    	when '1'
-	    		# plays high low
 	    		@high_low = HighLow.new
 	    		@high_low.deal_cards
-	    		payout_high_low
+	    		Cashier.payout_high_low(@high_low, @player)
 	    	when '2'
 	    		@slots = Slots.new
 	    		@slots.pull
-	    		# help us reset the instance of our game
-	    		# because high_low is already instantiated
-	    		# so it always hits the 'if' in payout
-	    		payout_slots
+	    		Cashier.payout_slots(@slots, @player)
 	    	else
 	    		puts "Goodbye!"
 	    		exit
     	end
+    	play_again
     end
 
-    def payout_high_low
-		if @high_low.player_wins == true 
-			@player.bank_roll = @player.bank_roll + @high_low.wager 
-			printf("Your new bank roll is $%.2f.\n", @player.bank_roll)
-		elsif @high_low.player_wins == false
-			  @player.bank_roll = @player.bank_roll - @high_low.wager
-			  printf("Your new bank roll is $%.2f.\n", @player.bank_roll)
-		end
-    	if @player.bank_roll == 0 
-    		exit(0) 
-    	end
+	def play_again
+	    puts "Would you like to play again? (y/n)"
+		user_input = gets.strip.downcase
+		hit_the_tables if user_input == 'y'
+	end
 
-	    	puts "Would you like to play again? (y/n)"
-			user_input = gets.strip.downcase
-		# TODO put this on one line
-		if user_input == 'y'
-			hit_the_tables
-		else
-		end
-    end
-
-  def payout_slots
-		if @slots.player_wins == true
-			@player.bank_roll = @player.bank_roll + @slots.wager 
-			printf("Your new bank roll is $%.2f.\n", @player.bank_roll)
-    	elsif @slots.player_wins == false
-    		  @player.bank_roll = @player.bank_roll - @slots.wager 
-    		  printf("Your new bank roll is $%.2f.\n", @player.bank_roll)
-		end
-    	if @player.bank_roll == 0 
-    		exit(0) 
-    	end
-
-	    	puts "Would you like to play again? (y/n)"
-			user_input = gets.strip.downcase
-		# TODO put this on one line
-		if user_input == 'y'
-			hit_the_tables
-		else
-		end
-    end
-
-    	
     def hit_the_tables
     	pick_game
     end
+
+  #   def payout_high_low
+		# if @high_low.player_wins == true 
+		# 	@player.bank_roll = @player.bank_roll + @high_low.wager
+		# 	printf("Your new bank roll is $%.2f.\n", @player.bank_roll)
+		# elsif @high_low.player_wins == false
+		# 	  @player.bank_roll = @player.bank_roll - @high_low.wager
+		# 	  printf("Your new bank roll is $%.2f.\n", @player.bank_roll)
+		# end
+  #   	exit(0) if @player.bank_roll == 0
+	 #    puts "Would you like to play again? (y/n)"
+		# user_input = gets.strip.downcase
+		# hit_the_tables if user_input == 'y'
+  #   end
+
+  # 	def payout_slots
+		# if @slots.player_wins == true
+		# 	@player.bank_roll = @player.bank_roll + @slots.wager
+		# 	printf("Your new bank roll is $%.2f.\n", @player.bank_roll)
+  #   	elsif @slots.player_wins == false
+  #   		  @player.bank_roll = @player.bank_roll - @slots.wager 
+  #   		  printf("Your new bank roll is $%.2f.\n", @player.bank_roll)
+		# end
+  #   	exit(0) if @player.bank_roll == 0
+	 #    puts "Would you like to play again? (y/n)"
+		# user_input = gets.strip.downcase
+		# hit_the_tables if user_input == 'y'
+  #   end
+
+    	
+   
 end
 
 
@@ -101,11 +92,34 @@ class Player
 
 end
 
+
+class Cashier
+  	def self.payout_high_low(high_low, player)
+		if high_low.player_wins == true 
+			player.bank_roll = player.bank_roll + high_low.wager
+			printf("Your new bank roll is $%.2f.\n", player.bank_roll)
+		elsif high_low.player_wins == false
+			  player.bank_roll = player.bank_roll - high_low.wager
+			  printf("Your new bank roll is $%.2f.\n", player.bank_roll)
+		end
+    	exit(0) if player.bank_roll == 0
+    end
+
+  	def self.payout_slots(slots, player)
+		if slots.player_wins == true
+			player.bank_roll = player.bank_roll + slots.wager
+			printf("Your new bank roll is $%.2f.\n", player.bank_roll)
+    	elsif slots.player_wins == false
+    		  player.bank_roll = player.bank_roll - slots.wager 
+    		  printf("Your new bank roll is $%.2f.\n", player.bank_roll)
+		end
+    	exit(0) if player.bank_roll == 0
+    end
+end
+
 Casino.new.hit_the_tables
 
 
 # TO DO list
-# break out payouts into different methods 
-# create a cashier class
 # ask for bankroll, set user input equal to bankroll
 # dynamic wager (choose how much to bet, give option for all in)
