83e2ef77dca8d7881f87436f540e995cd38cee31
Tom Lynam <tom.s.lynam@gmail.com>
2016-05-16 18:45:46 -0600

    slots as separate file

---
+++ slots.rb
@@ -0,0 +1,37 @@
+# require_relative 'mechanics/dice'
+Dir[File.dirname(__FILE__) + '/mechanics/*.rb'].each {|file| require file }
+require 'pry'
+
+class Slots
+	attr_accessor :wager, :player_wins
+
+	def initialize
+		@wager = 10
+	end
+
+ 	def pull
+  	
+		# generate random number between 0 and 9
+		wheel1 = rand(0..9)
+		wheel2 = rand(0..9)
+		wheel3 = rand(0..9)
+		numbers = [wheel1, wheel2, wheel3]
+		puts numbers.join(' ')
+
+		# binding.pry
+		if numbers.uniq.count == 1
+			puts "Jackpot!"
+			# @player_wins = true
+			# maybe also add a bonus here in addition to doubling the wager
+		elsif numbers.uniq.count == 2
+			puts "Winnner winner!"
+			# @player_wins = false
+		else 
+			puts "Sorry, not a winner."
+			# @player_wins = false
+		end
+		
+	end  
+end
+
+Slots.new.pull
